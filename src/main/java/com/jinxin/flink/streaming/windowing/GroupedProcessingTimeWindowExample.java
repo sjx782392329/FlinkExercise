0680d8f45553665a5175b4db6f66b4618ff0cbcc
jinxin <782392329@qq.com>
2019-12-26 18:43:01 +0800

    Flink Session 窗口，大于 gap 才会进入下一个新的窗口

---
+++ src/main/java/com/jinxin/flink/streaming/windowing/GroupedProcessingTimeWindowExample.java
@@ -0,0 +1,62 @@
+package com.jinxin.flink.streaming.windowing;
+
+import org.apache.flink.api.common.functions.ReduceFunction;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.sink.SinkFunction;
+import org.apache.flink.streaming.api.functions.source.RichParallelSourceFunction;
+import org.apache.flink.streaming.api.windowing.time.Time;
+
+import java.util.concurrent.TimeUnit;
+
+public class GroupedProcessingTimeWindowExample {
+    public static void main(String[] args) throws Exception {
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(4);
+
+        DataStream<Tuple2<Long, Long>> stream = env.addSource(new DataSource());
+        stream
+            .keyBy(0)
+                .timeWindow(Time.of(2500, TimeUnit.MILLISECONDS), Time.of(500, TimeUnit.MILLISECONDS))
+                .reduce(new SumingReducer())
+                .addSink(new SinkFunction<Tuple2<Long, Long>>() {
+                    public void invoke(Tuple2<Long, Long> value) throws Exception {
+                    }
+                });
+        env.execute();
+    }
+
+    private static class DataSource extends RichParallelSourceFunction<Tuple2<Long, Long>> {
+        private volatile boolean running = true;
+        public void run(SourceContext<Tuple2<Long, Long>> sourceContext) throws Exception {
+            final long startTime = System.currentTimeMillis();
+
+            final long numElements = 20000000;
+            final long numKeys = 10000;
+            long val = 1L;
+            long count = 0L;
+
+            while (running && count < numElements){
+                count++;
+                sourceContext.collect(new Tuple2<Long, Long>(val++, 1L));
+
+                if (val > numKeys){
+                    val = 1L;
+                }
+            }
+            final long endTime = System.currentTimeMillis();
+            System.out.println("Took " + (endTime - startTime) + "msecs for " + numElements + " values");
+        }
+
+        public void cancel() {
+            running = false;
+        }
+    }
+
+    private static class SumingReducer implements ReduceFunction<Tuple2<Long, Long>> {
+        public Tuple2<Long, Long> reduce(Tuple2<Long, Long> value1, Tuple2<Long, Long> value2) throws Exception {
+            return new Tuple2<Long, Long>(value1.f0, value1.f1 + value2.f1);
+        }
+    }
+}
---
+++ src/main/java/com/jinxin/flink/streaming/windowing/SessionWindowing.java
@@ -0,0 +1,77 @@
+package com.jinxin.flink.streaming.windowing;
+
+import org.apache.flink.api.java.tuple.Tuple3;
+import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.streaming.api.TimeCharacteristic;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.source.SourceFunction;
+import org.apache.flink.streaming.api.watermark.Watermark;
+import org.apache.flink.streaming.api.windowing.assigners.EventTimeSessionWindows;
+import org.apache.flink.streaming.api.windowing.time.Time;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/***
+ *  Session window 在时间戳差值严格大于 gap 的时候才会触发新的窗口操作
+ *  等于 gap 的时候还是会被计算在旧的窗口操作
+  */
+public class SessionWindowing {
+    public static void main(String[] args) throws Exception {
+        final ParameterTool params = ParameterTool.fromArgs(args);
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.getConfig().setGlobalJobParameters(params);
+        env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
+        env.setParallelism(1);
+
+        final boolean fileOutput = params.has("output");
+
+        final List<Tuple3<String, Long, Integer>> input = new ArrayList<Tuple3<String, Long, Integer>>();
+
+        input.add(new Tuple3<String, Long, Integer>("a", 1L, 1));
+
+        input.add(new Tuple3<String, Long, Integer>("b", 1L, 1));
+        input.add(new Tuple3<String, Long, Integer>("b", 3L, 1));
+        input.add(new Tuple3<String, Long, Integer>("b", 5L, 1));
+        input.add(new Tuple3<String, Long, Integer>("c", 6L, 1));
+
+        input.add(new Tuple3<String, Long, Integer>("b", 7L, 1));
+        input.add(new Tuple3<String, Long, Integer>("b", 9L, 1));
+
+        input.add(new Tuple3<String, Long, Integer>("b", 12L, 1));
+        input.add(new Tuple3<String, Long, Integer>("b", 16L, 1));
+
+        input.add(new Tuple3<String, Long, Integer>("a", 10L, 1));
+        input.add(new Tuple3<String, Long, Integer>("c", 11L, 1));
+        input.add(new Tuple3<String, Long, Integer>("6", 11L, 1));
+
+        DataStream<Tuple3<String, Long, Integer>> source = env
+                .addSource(new SourceFunction<Tuple3<String, Long, Integer>>() {
+                    private static final long serialVersionUID = 1L;
+                    public void run(SourceContext<Tuple3<String, Long, Integer>> sourceContext) throws Exception {
+                        for (Tuple3<String, Long, Integer> value : input){
+                            sourceContext.collectWithTimestamp(value, value.f1);
+                            sourceContext.emitWatermark(new Watermark(value.f1 - 1));
+                        }
+                        sourceContext.emitWatermark(new Watermark(Long.MAX_VALUE));
+                    }
+
+                    public void cancel() {
+
+                    }
+                });
+        DataStream<Tuple3<String, Long, Integer>> aggregated = source
+                .keyBy(0)
+                .window(EventTimeSessionWindows.withGap(Time.milliseconds(3L)))
+                .sum(2);
+
+        if (fileOutput){
+            aggregated.writeAsText(params.get("output"));
+        } else {
+            System.out.println("Printing result to stdout");
+            aggregated.print();
+        }
+        env.execute();
+    }
+}
